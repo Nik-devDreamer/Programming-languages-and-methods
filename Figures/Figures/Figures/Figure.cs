8a8948deea0416f1dd731d626b7c20b0b9288683
Nikita <80746832+Nikita-Programmer@users.noreply.github.com>
2021-11-09 23:51:10 +0200

    Add files via upload

---
+++ Figures/Figures/Figures/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramFigures.Figures
+{
+    public abstract class Figure
+    {
+        protected Random random = new Random();
+
+        public abstract string Verification();
+
+        public abstract double Perimetr();
+
+        public abstract double Square();
+
+        public abstract void GetInfo();
+
+        public int CompareTo(object obj)
+        {
+            int index = Perimetr().CompareTo(((Figure)obj).Perimetr());
+            return index;
+        }
+    }
+}
---
+++ Figures/Figures/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using ProgramFigures.Figures;
+
+namespace ProgramFigures
+{
+    class Program
+    {
+        static void Main()
+        {
+            int InputData, countFigure;
+            List<Figure> figures = new List<Figure>();
+            Random random = new Random();
+            Figure figure;
+
+            while (true)
+            {
+                Console.Write("Введите количество фигур: ");
+                countFigure = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Введите номер фигуры, которая вам необходима");
+                try
+                {
+                    Console.WriteLine("1 - Прямоугольник;\n2 - Круг;\n3 - Треугольник\nДругая цифра - выход.");
+                    InputData = Int32.Parse(Console.ReadLine());
+                    if (InputData == 1)
+                    {
+                        for (int i = 0; i < countFigure; i++)
+                        {
+                            figure = new Rectangle(i + 1);
+                            figures.Add(figure);
+                        }
+                    }
+                    else if (InputData == 2)
+                    {
+                        for (int i = 0; i < countFigure; i++)
+                        {
+                            figure = new Ellipse(i + 1);
+                            figures.Add(figure);
+                        }
+                    }
+                    else if (InputData == 3)
+                    {
+                        for (int i = 0; i < countFigure; i++)
+                        {
+                            figure = new Triangle(i + 1);
+                            figures.Add(figure);
+                        }
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+
+                    Console.WriteLine("Первоначальный список:");
+                    foreach (Figure x in figures)
+                    {
+                        x.GetInfo();
+                    }
+                    Console.WriteLine();
+
+                    figures.Sort((a, b) => a.Perimetr().CompareTo(b.Perimetr()));
+                    IEnumerator IEnum = figures.GetEnumerator();
+
+                    Console.WriteLine("Отсортированный список 'foreach'");
+                    foreach (Figure x in figures)
+                    {
+                        x.GetInfo();
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Отсортированный список 'while'");
+                    while (IEnum.MoveNext())
+                    {
+                        figure = (Figure)IEnum.Current;
+                        figure.GetInfo();
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Хотите повторить?\n1 - Да;\nДругое число - Нет");
+                    int g = int.Parse(Console.ReadLine());
+                    if (g == 1)
+                    {
+                        Console.Clear();
+                        Main();
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                    Main();
+                }
+            }
+        }
+    }
+}
